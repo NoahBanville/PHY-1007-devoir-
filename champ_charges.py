2f0c98b9a95b0d9529fd4402e4af4779c9a100be
Noah <noban1@ulaval.ca>
2024-01-25 11:45:45 -0500

    mettre code de la question a) au propre

--- champ_charges.py
+++ champ_charges.py
@@ -1,8 +1,6 @@
-# Affichage du champ de charge électriques discrètes en 2D
-
+#devoir 1/PHY-1007/Noah Banville et Olivier Lebel
 # %%
 from typing import TypeAlias
-
 import numpy as np
 from numpy import pi
 from scipy.constants import epsilon_0
@@ -36,14 +34,10 @@ def calcul_E(x,y,charge:Charge):
    return Ex, Ey
 
 def champ_charges(charges:list[Charge]):
-   """ Calcul le champ vectoriel d'un ensemble de charges
-
-       Placées dans un 'monde' allant de -1 à 1 en x et y
-   """
-
+   """ Calcul le champ vectoriel d'un ensemble de charges"""
    # Definissons le 'monde'
-   x = np.linspace(-1,5,200)
-   y = np.linspace(-2,2,200)
+   x = np.linspace(-1,5,400)
+   y = np.linspace(-2,2,400)
    X,Y = np.meshgrid(x,y)
 
    Ex = np.zeros(X.shape)
@@ -58,11 +52,26 @@ def champ_charges(charges:list[Charge]):
    P = np.sqrt(Ex**2 + Ey**2) # magnitude du champ
    return X, Y, Ex, Ey, P
 
+def magnitude_x(charges, y=0, z=0):
+    """Calcul de la magnitude du champ électrique en fonction de x( question a) )."""
+    x_val = np.linspace(0, 5, 400)
+    magnitude_val = np.zeros_like(x_val)
+
+    for i, x in enumerate(x_val):
+        Ex, Ey = 0, 0
+        for charge in charges:
+            I, J = calcul_E(x, y, charge)
+            Ex += I
+            Ey += J
+        magnitude_val[i] = np.sqrt(Ex**2 + Ey**2)
+
+    return x_val, magnitude_val
+
 def affiche_graph(charges:list[Charge],title="",saveName=None):
     """ calcul et affiche le champ """
     X, Y, Ex, Ey, P = champ_charges(charges)
    
-    #Construisons la figure des lignes de champs
+    # figure des lignes de champs
     fig, (ax1, ax2) = plt.subplots(1,2, figsize=(16,6))
     
     pax = ax1.streamplot(X, Y, Ex, Ey, color=np.log(P), density=1.2 ,linewidth=2,\
@@ -76,7 +85,7 @@ def affiche_graph(charges:list[Charge],title="",saveName=None):
     ax1.set_aspect('equal')
     ax1.set_title("Lignes de champ des charges")
     
-    #figure de la magnétidue du champ selon x
+    #figure de la magnétude du champ selon x
     x_val, magnitude_val = magnitude_x(charges)
     ax2.plot(x_val, magnitude_val)
     ax2.set_xlabel(r'$x$')
@@ -89,26 +98,11 @@ def affiche_graph(charges:list[Charge],title="",saveName=None):
     if saveName:
         plt.savefig(saveName)
     plt.show()
-# %%
-def magnitude_x(charges, y=0, z=0):
-    """Calcul de la magnitude du champ électrique en fonction de x."""
-    x_val = np.linspace(0, 5, 200)
-    magnitude_val = np.zeros_like(x_val)
 
-    for i, x in enumerate(x_val):
-        Ex, Ey = 0, 0
-        for charge in charges:
-            I, J = calcul_E(x, y, charge)
-            Ex += I
-            Ey += J
-        magnitude_val[i] = np.sqrt(Ex**2 + Ey**2)
-
-    return x_val, magnitude_val
-
-
+#charges utilisées pour l'exercice
 c = [Charge((2.5,1.8),1.5), Charge((0,1.2),0.3), Charge((2,-1.5),0.5), Charge((0.6,-1.2),-0.5)]
 
-#affiche les graphs
+#affiche les 2 graphs
 affiche_graph(c)
 
 
