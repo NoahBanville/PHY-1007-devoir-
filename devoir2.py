5571ca3fa12d23ab8cd3d4ab4c54b02f2fd80049
Olivier Lebel <olleb25@ulaval.ca>
2024-02-11 09:29:49 -0500

    -

--- devoir2.py
+++ devoir2.py
@@ -8,8 +8,6 @@ from scipy.constants import epsilon_0
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
 
-from scipy.integrate import quad
-
 vect3D: TypeAlias = (float, float, float)
 class Charge:
    def __init__(self,position:vect3D,charge:float):
