3f37eff0322121cbf5670ad6b71774f04f95d3c1
Noah <noban1@ulaval.ca>
2024-02-11 17:23:31 -0500

    import module legendre

--- devoir2.py
+++ devoir2.py
@@ -5,6 +5,7 @@ from typing import TypeAlias
 import numpy as np
 from numpy import pi
 from scipy.constants import epsilon_0
+from numpy.polynomial import legendre
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
 
@@ -15,6 +16,17 @@ class Charge:
       self.y = position[1]
       self.z = position[2]
       self.q = charge
+      
+"""def potentiel_E(x, y, z, charges):
+    potentiel = 0
+    for charge in charges:
+        r = np.sqrt((x - charge.x)**2 + (y - charge.y)**2 + (z - charge.z)**2)
+        potentiel += charge.q / (4 * pi * epsilon_0 * r)
+    return potentiel"""
 
 #charges utilisées pour l'exercice
 c = [Charge((5, 5, 0), 1.0), Charge((-5, 5, 0), -1.0), Charge((5, -5, 0), -1.0), Charge((-5, -5, 0), 1.0)]
+
+def exp_multipolaire():
+   pass
+      
