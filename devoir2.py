e9be906b97e3f23aee6ec0fc0f887e42362da05e
Olivier Lebel <olleb25@ulaval.ca>
2024-02-13 10:14:56 -0500

    exp_multipolaire

--- devoir2.py
+++ devoir2.py
@@ -8,6 +8,7 @@ from scipy.constants import epsilon_0
 from numpy.polynomial import legendre
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
+import math
 
 vect3D: TypeAlias = (float, float, float)
 class Charge:
@@ -19,7 +20,17 @@ class Charge:
 
 def exp_multipolaire(x, y, z, c):
     pot = [0, 0, 0, 0, 0, 0, 0]
-
+    
+    for i in range(6):
+        pot[i] += (1/(np.sqrt(x**2 + y**2 + z**2))**(i+1))
+        n = [0, 0, 0, 0, 0, 0]
+        n[i] = 1
+        for k in c:
+            angle = (k.x*x + k.y*y + k.z*z)/(math.sqrt(k.x**2 + k.y**2 + k.z**2)*math.sqrt(x**2 + y**2 + z**2))
+            pot[i] *= ((np.sqrt(k.x**2 + k.y**2 + k.z**2))**i) * (legendre.legval(angle, n)) * k.q
+        pot[i] *= (1/(4*np.pi*epsilon_0))
+        pot[6] += pot[i]
+    #Les 6 première cases du tableau sont les expansions pour n=0, 1, 2, 3, 4, 5 et la dernière case est la somme des 6. 
     return pot
 
 def potentiel_E(x, y, z, charges):
@@ -31,7 +42,3 @@ def potentiel_E(x, y, z, charges):
 
 #charges utilisées pour l'exercice
 c = [Charge((5, 5, 0), 1.0), Charge((-5, 5, 0), -1.0), Charge((5, -5, 0), -1.0), Charge((-5, -5, 0), 1.0)]
-
-
-
-print(exp_multipolaire(23, 32, 50, c))
