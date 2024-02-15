0a9454877bac4d45eaf628bf818f6fdbfdd40504
Noah <noban1@ulaval.ca>
2024-02-15 10:16:01 -0500

    .

--- devoir2.py
+++ devoir2.py
@@ -7,49 +7,46 @@ from numpy import pi, sqrt
 from scipy.constants import epsilon_0
 from numpy.polynomial import legendre
 from matplotlib import pyplot as plt
+from matplotlib import rcParams
+import math
 
 vect3D: TypeAlias = (float, float, float)
-
 class Charge:
-    def __init__(self, position: vect3D, charge: float):
-        self.position = np.array(position)
-        self.q = charge
+   def __init__(self,position:vect3D,charge:float):
+      self.x = position[0]
+      self.y = position[1]
+      self.z = position[2]
+      self.q = charge
 
 def exp_multipolaire(x, y, z, c):
     pot = np.zeros(7)
-    sqrt_abs_x = sqrt(np.abs(x))
-    sqrt_abs_y = sqrt(np.abs(y))
-    sqrt_abs_z = sqrt(np.abs(z))
-    sqrt_abs_xyz = sqrt_abs_x**2 + sqrt_abs_y**2 + sqrt_abs_z**2
-    
     for i in range(6):
         n = np.zeros(6)
         n[i] = 1
         for k in c:
-            angle = (k.position[0]*x + k.position[1]*y + k.position[2]*z) / \
-                    (sqrt(k.position[0]**2 + k.position[1]**2 + k.position[2]**2) * sqrt_abs_xyz)
-            legendre_terme = legendre.legval(angle, n, tensor=True)
-            pot[i] += k.q * legendre_terme * (
-                                                sqrt(np.abs(k.position[0])**2 + 
-                                                     np.abs(k.position[1])**2 + 
-                                                     np.abs(k.position[2])**2
-                                                     ))**i
+            angle = (k.x*x + k.y*y + k.z*z)/(sqrt(k.x**2 + k.y**2 + k.z**2)*sqrt(x**2 + y**2 + z**2))
+            pot[i] += k.q * legendre.legval(angle, n, tensor=True) * (sqrt(np.absolute(k.x)**2 + np.absolute(k.y)**2 + np.absolute(k.z) **2))**i
         
-        pot[i] *= (1/(4*pi*epsilon_0)) * (1/sqrt_abs_xyz)**(i+1)
+        pot[i] *= (1/(4*pi*epsilon_0)) * (1/(sqrt(np.absolute(x)**2 + np.absolute(y)**2 + np.absolute(z)**2))**(i+1))
         pot[6] += pot[i]
-    
+    #Les 6 première cases du tableau sont les expansions pour n = 0, 1, 2, 3, 4, 5 et la dernière case est la somme des 6.
     return pot
+####
+
 
 def affiche_graph(c):
-    X, Y = np.meshgrid(np.arange(-100, 101), np.arange(-100, 101))
-    Z = np.zeros((201, 201, 6))
 
-    for i in range(1,6):
+    # make data
+    X, Y = np.meshgrid(np.arange(-100, 101), np.arange(-100, 101))
+    Z = np.zeros((201, 201))
+    for i in np.arange(-100, 101):
         for j in np.arange(-100, 101):
-            Z[i][j] = exp_multipolaire(i*(10**(-9)), j*(10**(-9)), 50*(10**(-9)), c)[1]
+            Z[j][i] = exp_multipolaire(i*(10**(-9)), j*(10**(-9)), 50*(10**(-9)), c)[2]
     levels = np.linspace(Z.min(), Z.max(), 100)
 
-    fig, ax1 = plt.subplots()
+    # plot
+    fig, ax1= plt.subplots(layout='constrained')
+
     cs = ax1.contourf(X, Y, Z, levels=levels)
     cbar = fig.colorbar(cs)
 
@@ -60,11 +57,11 @@ def affiche_graph(c):
     
 # Charges used for the exercise
 c = [
-    Charge((5*(10**(-9)), 5*(10**(-9)), 0), 1.0*(10**(-12))), 
-    Charge((-5*(10**(-9)), 5*(10**(-9)), 0), -1.0*(10**(-12))),
-    Charge((5*(10**(-9)), -5*(10**(-9)), 0), -1.0*(10**(-12))), 
-    Charge((-5*(10**(-9)), -5*(10**(-9)), 0), 1.0*(10**(-12)))
+    Charge((5e-9, 5e-9, 0), 1.0e-12),
+    Charge((-5e-9, 5e-9, 0), -1.0e-12),
+    Charge((5e-9, -5e-9, 0), -1.0e-12),
+    Charge((-5e-9, -5e-9, 0), 1.0e-12)
 ]
 
-print(exp_multipolaire(43*(10**(-9)), 23*(10**(-9)), 50*(10**(-9)), c))
+print(exp_multipolaire(43e-9, 23e-9, 50e-9, c))
 affiche_graph(c)  # Uncomment to display the graph
